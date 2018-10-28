d6c3ff763a49f975cd59c79856213397723d9a92
nullorone <sy.v@mail.ru>
2018-10-29 00:30:36 +0300

    Стилизует апи Яндекс карт

---
+++ source/js/ymap.js
@@ -0,0 +1,36 @@
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [59.93870912, 30.3231457],
+    zoom: 17
+  });
+
+  var myPlacemark = new ymaps.Placemark([59.93870912, 30.3231457], {}, {
+    iconLayout: "default#image",
+    iconImageHref: "img/icon-map-pin.svg",
+    iconImageSize: [66, 101],
+    iconImageOffset: [-40, -90],
+    balloonContentHeader:
+      "<a class='logo' style='margin-left: auto; margin-right: auto'></a>",
+    balloonContentBody:
+      "<h2 style='text-align: center'>Милые штуки ручной<br> работы для дома</h2>"
+  });
+  myMap.geoObjects.add(myPlacemark);
+
+  myMap.behaviors.disable([
+    "drag",
+    "dblClickZoom",
+    "rightMouseButtonMagnifier",
+    "ruler",
+    "routeEditor",
+    "scrollZoom"
+  ]);
+  myMap.controls
+    .remove("searchControl")
+    .remove("routeButtonControl")
+    .remove("trafficControl")
+    .remove("typeSelector")
+    .remove("fullscreenControl")
+    .remove("rulerControl");
+}
